f02ab3face44c1e6715a1f1578832bb105a98021
warenr02 <warenr02@gmail.com>
2025-09-28 17:34:40 +0300

    Tasks 1 and 2

---
+++ Tasks.ts
@@ -0,0 +1,11 @@
+// Task 1: Calculate the area of a rectangle
+let rectangleLength = 2;
+let rectangleWidth = 5;
+let area = rectangleLength * rectangleWidth;
+console.log( "The area of rectangle is ", area);
+
+// Task 2: Calculate Body Mass Index (BMI)
+let height = 1.65;
+let weight = 50;
+let bmi = weight / (height * height)
+console.log ("Bmi is ", bmi);
