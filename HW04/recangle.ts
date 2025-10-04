3823087787a2269b4527565ace85794125369456
warenr02 <warenr02@gmail.com>
2025-10-04 10:16:00 +0300

    rectangle.ts file is fixed

---
+++ HW04/recangle.ts
@@ -0,0 +1,16 @@
+class Rectangle {
+    width: number
+    height: number
+
+    constructor(width: number, height: number) {
+        this.width = width
+        this.height = height
+    }
+
+    getArea(): number {
+        return this.width * this.height
+    }
+}
+
+const rect = new Rectangle(5, 10)
+console.log(rect.getArea()) // After fixing: will print 50
