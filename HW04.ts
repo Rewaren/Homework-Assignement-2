151a39e8103dfe02a20ddfd8a7beaed75482f65a
warenr02 <warenr02@gmail.com>
2025-10-04 10:01:05 +0300

    Hw 04 product class added

--- HW04.ts
+++ HW04.ts
@@ -9,6 +9,28 @@ class Calculator{
     }
 }
 
+class Product {
+    name: string;
+    price: number;
+    quantity: number;
+
+    constructor(name, price, quantity) {
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+    }
+
+    isInStock(): boolean{
+        return this.quantity > 0;
+    }
+}
+
+
 const calculator1 = new Calculator();
 console.log(calculator1.addition(1,2));
 console.log(calculator1.isGreeter(1,2));
+
+const product1 = new Product("Apples", 2,5);
+console.log(product1.isInStock());
+const product2 = new Product("Oranges", 6,0);
+console.log(product2.isInStock());
