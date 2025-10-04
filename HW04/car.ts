da37a8ed7197a6c784d1b7d36c502a5d10ab9476
warenr02 <warenr02@gmail.com>
2025-10-04 10:13:01 +0300

    car.ts file is fixed

---
+++ HW04/car.ts
@@ -0,0 +1,17 @@
+class Car {
+    brand: string;
+    speed: number;
+
+    constructor(brand: string, speed: number) {
+        this.brand = brand;
+        this.speed = speed;
+    }
+
+    accelerate(): void {
+        this.speed += 20;
+    }
+}
+
+const car = new Car("Toyota", 100);
+car.accelerate();
+console.log(car.speed); // After fixing: will print 120
